545f45190b52af162650973f002ab424f4c17124
Shriman Sourav Sen <souravsens2010@gmail.com>
2023-06-08 02:45:07 +0530

    approval screen data updated

--- src/app/approval-management/approval-management.component.ts
+++ src/app/approval-management/approval-management.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./approval-management.component.scss']
 })
 export class ApprovalManagementComponent {
-  approvalObj: any[] = [
+  leaveApprovalObj: any[] = [
     {name: "Steve Rogers", startDate: '12/04/2023', type: 'Earned', duration: 2},
     {name: "Clark Kent", startDate: '21/02/2023', type: 'Sick', duration: 3},
     {name: "John Doe", startDate: '04/03/2023', type: 'Annual', duration: 5},
@@ -14,5 +14,16 @@ export class ApprovalManagementComponent {
     {name: "Robert D'Costa", startDate: '23/04/2023', type: 'Optional', duration: 1},
     {name: "Bruce Wayne", startDate: '12/05/2023', type: 'Emergency', duration: 0.5},
     {name: "Peter Parker", startDate: '06/02/2023', type: 'Emergency', duration: 0.5}
+  ];
+  travelApprovalObj: any[] = [
+    {name: "Steve Rogers", startDate: '12/04/2023', projectName: 'ABC', duration: 180},
+    {name: "Clark Kent", startDate: '21/02/2023', projectName: 'ABC', duration: 90},
+    {name: "John Doe", startDate: '04/03/2023', projectName: 'XYZ', duration: 365}
+  ];
+  expenseApprovalObj: any[] = [
+    {name: "Steve Rogers", expenseDate: '12/04/2023', expensePurpose: 'Client Lunch', amount: 2000, amountType: 'INR', projectName: 'ABC'},
+    {name: "Clark Kent", expenseDate: '21/02/2023',  expensePurpose: 'Team Lunch', amount: 2000, amountType: 'Dollar', projectName: 'XYZ'},
+    {name: "John Doe", expenseDate: '04/03/2023',  expensePurpose: 'Client Gift', amount: 2000, amountType: 'Pound', projectName: 'PQR'},
+    {name: "Stefen Parker", expenseDate: '31/05/2023',  expensePurpose: 'Project Branding', amount: 2000, amountType: 'INR', projectName: 'ABC'}
   ];
 }
